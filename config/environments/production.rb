4ff2ab3db4db00ac52be211301e92ec4f11d9b6d
justinkhor <khorchengee@gmail.com>
2017-06-09 15:05:20 +0800

    minor edits

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,6 +101,6 @@ end
  config.action_mailer.perform_deliveries = true
  config.action_mailer.raise_delivery_errors = true
  config.action_mailer.default_options = {from: 'justinairbnbclone@gmail.com'}
- config.action_mailer.default_url_options = { host: 'https://my-airbnb-clone.herokuapp.com/' }
+ config.action_mailer.default_url_options = { host: 'https://my-airbnb-clone.herokuapp.com' }
 
 end
